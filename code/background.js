9f1fba5b2804409a072b1bde9a6a66972a9a38fc
Nolen Royalty <nolen.royalty@gmail.com>
2024-03-12 16:45:10 -0400

    add my own images, delete old background file

--- code/background.js
+++ code/background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
-    text: "OFF",
+    text: "",
   });
 });
 
@@ -14,16 +14,16 @@ chrome.action.onClicked.addListener(async (tab) => {
     nextState = prevState === "ON" ? "OFF" : "ON";
     await chrome.action.setBadgeText({
       tabId: tab.id,
-      text: nextState,
+      text: nextState === "ON" ? "ON" : "",
     });
-  }
 
-  const cssArgs = { files: ["style.css"], target: { tabId: tab.id } };
-  const jsArgs = { files: ["main.js"], target: { tabId: tab.id } };
-  if (nextState === "ON") {
-    await chrome.scripting.insertCSS(cssArgs);
-    await chrome.scripting.executeScript(jsArgs);
-  } else if (nextState === "OFF") {
-    await chrome.scripting.removeCSS(cssArgs);
+    const cssArgs = { files: ["style.css"], target: { tabId: tab.id } };
+    const jsArgs = { files: ["main.js"], target: { tabId: tab.id } };
+    if (nextState === "ON") {
+      await chrome.scripting.insertCSS(cssArgs);
+      await chrome.scripting.executeScript(jsArgs);
+    } else if (nextState === "OFF") {
+      await chrome.scripting.removeCSS(cssArgs);
+    }
   }
 });
